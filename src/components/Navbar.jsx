d477c44048457cedcc9f88ff006cdd9c9cb740ee
belalhanafy <belalhanafy373@gmail.com>
2025-09-22 12:10:19 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md dark:bg-gray-800 transition-colors">
-            <div className="flex items-center gap-3">
-                <img src="/Wordle_Logo.svg.png" alt="Wordy Logo" className="w-8 h-8" />
-                <h1 className="text-2xl font-bold tracking-wide text-green-600 dark:text-green-400">
+            <div className="flex items-center lg:gap-3 gap-1">
+                <img src="/Wordle_Logo.svg.png" alt="Wordy Logo" className="w-5 h-5 lg:w-8 lg:h-8" />
+                <h1 className="lg:text-2xl text-xl font-bold tracking-wide text-green-600 dark:text-green-400">
                     Wordy
                 </h1>
             </div>
 
-            <div className="flex items-center gap-4">
-                <button onClick={() => setHelp(!help)} className="px-3 py-1 rounded-lg bg-gray-200 text-gray-800 hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600 transition">
+            <div className="flex items-center lg:gap-4 gap-2">
+                <button onClick={() => setHelp(!help)} className="text-sm lg:text-lg px-3 py-1 rounded-lg bg-gray-200 text-gray-800 hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600 transition">
                     Help
                 </button>
                 
                 <button
                     onClick={giveHint}
                     disabled={hintsLeft <= 0}
-                    className={`px-3 py-1 rounded-lg font-medium transition 
+                    className={`px-3 py-1 rounded-lg font-medium transition text-sm lg:text-lg
                         ${hintsLeft > 0
                             ? "bg-blue-500 text-white hover:bg-blue-600"
                             : "bg-gray-400 text-gray-200 cursor-not-allowed"}`}
